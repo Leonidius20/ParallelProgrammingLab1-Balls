fb13a455fb2cca9d44bd84640076574acbf2b27d
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2022-02-23 23:51:28 +0200

    implemented 5th task

--- src/main/java/Bounce.java
+++ src/main/java/Bounce.java
@@ -1,13 +1,64 @@
-import javax.swing.*;
+import org.w3c.dom.xpath.XPathResult;
 
 public class Bounce {
-    public static void main(String[] args) {
-        BounceFrame frame = new BounceFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.setVisible(true);
-        System.out.println("Thread name = " +
-                Thread.currentThread().getName());
+    public boolean flag = false;
 
+    public static void main(String[] args) throws InterruptedException {
+
+        boolean b = true;
+
+        Thread hyphen;
+
+        var ob = new Bounce();
+
+        var bar = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                ob.printBar();
+            }
+        });
+
+        hyphen = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                for (int j = 0; j < 10; j++) {
+                    ob.printHyphen();
+                }
+                System.out.println();
+            }
+        });
+
+
+        bar.start();
+        hyphen.start();
+    }
+
+    public synchronized void printBar() {
+        while (flag) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("Thread Interrupted");
+            }
+        }
+        flag = true;
+
+        System.out.print("|");
+        notifyAll();
+    }
+
+    public synchronized void printHyphen() {
+        while (!flag) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("Thread Interrupted");
+            }
+        }
+        flag = false;
+
+        System.out.print("-");
+        notifyAll();
     }
 }
